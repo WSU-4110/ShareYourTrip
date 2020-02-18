c8abc146cbf25b2ade5df971a011564ecaa73055
qsphere <fx8925@wayne.edu>
2020-02-18 05:32:20 -0500

    Added userPost class and implementented default constructor, copy constructor, getter and setter methods

---
+++ app/src/main/java/com/example/shareyourtrip/UserPost.java
@@ -0,0 +1,65 @@
+package com.example.shareyourtrip;
+
+public class UserPost {
+    private String city;
+    private String state;
+    private String category;
+    private String title;
+    private String description;
+
+    public UserPost() {
+        this.city = "";
+        this.state = "";
+        this.category = "";
+        this.title = "";
+        this.description = "";
+    }
+
+    public UserPost(String city, String state, String category, String title, String description) {
+        this.city = city;
+        this.state = state;
+        this.category = category;
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
