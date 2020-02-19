fc14db77f33d6301b6ba4537bfde25fc010321f5
qsphere <fx8925@wayne.edu>
2020-02-19 14:06:08 -0500

    rediecting button from post page to post form 1

--- app/src/main/java/com/example/shareyourtrip/PostActivity.java
+++ app/src/main/java/com/example/shareyourtrip/PostActivity.java
@@ -6,15 +6,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class PostActivity extends AppCompatActivity {
 
+    // Variables for post page
+    Button create_post;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        create_post = (Button)findViewById(R.id.create_post);
+
+        create_post.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+                Intent post_page_intent = new Intent(PostActivity.this, PostForm1.class);
+                startActivity(post_page_intent);
+            }
+
+        });
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
 
