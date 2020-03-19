ed9fc657bd4a39bb5475b1353f773534e83e7fca
= <=>
2020-03-18 21:32:35 -0400

    Completed structure of FavoriteActivity

--- app/src/main/java/com/example/shareyourtrip/FavoriteActivity.java
+++ app/src/main/java/com/example/shareyourtrip/FavoriteActivity.java
@@ -2,23 +2,48 @@ package com.example.shareyourtrip;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/*
+        This activity is to pull the users favorite posts
+    and display them all in one Recycler view. The Recycler
+    uses UserPostAdapter as a blueprint for each row.
+ */
 
 public class FavoriteActivity extends AppCompatActivity {
 
-
+    //todo: Figure out how to reference users favList instead of using test list
+    private List<UserPost> favList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private UserPostAdapter postAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite);
 
-        //
+
+        //Assigning recycler view
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        postAdapter = new UserPostAdapter(favList);
+
+        //Setting up our recycler view
+        RecyclerView.LayoutManager postLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(postLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(postAdapter);
+
+        preparePostData();
 
         //Bottom Navigation
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
@@ -54,4 +79,23 @@ public class FavoriteActivity extends AppCompatActivity {
             }
         });
     }
+
+    //Used for Testing
+    private void preparePostData(){
+
+        UserPost post = new UserPost("Plymouth", "MI", "Park", "McClumpha", "Lit");
+        favList.add(post);
+
+        post = new UserPost("1", "2", "3", "4", "5");
+        favList.add(post);
+
+        post = new UserPost("1", "2", "3", "4", "5");
+        favList.add(post);
+
+        post = new UserPost("1", "2", "3", "4", "5");
+        favList.add(post);
+
+        postAdapter.notifyDataSetChanged();
+    }
+
 }
