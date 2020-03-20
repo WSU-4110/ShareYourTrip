18ddbcf90ae898975cec2680e79973ee410472ba
nonaeftekhari <fy4996@wayne.edu>
2020-03-20 18:27:16 -0400

    adding backend query for searching based on city and state

--- app/src/main/java/com/example/shareyourtrip/PostDAO.java
+++ app/src/main/java/com/example/shareyourtrip/PostDAO.java
@@ -153,20 +153,23 @@ public class PostDAO extends SQLiteOpenHelper {
         String user;
 
         //Post post;
+        int cc = cursor.getColumnCount();
+        int count = cursor.getCount();
+        if(count>0) {
+            cursor.moveToFirst();
+            do {
+                city = cursor.getString(1); //need to start one after the id column. if 0->1; if 1->2
+                state = cursor.getString(2);
+                category = cursor.getString(3);
+                title = cursor.getString(4);
+                description = cursor.getString(5);
+                user = cursor.getString(6);
 
-        do {
-            city = cursor.getString(1); //need to start one after the id column. if 0->1; if 1->2
-            state = cursor.getString(2);
-            category = cursor.getString(3);
-            title = cursor.getString(4);
-            description = cursor.getString(5);
-            user = cursor.getString(6);
+                Post post = new Post(city, state, category, title, description, user);
+                listPost.add(post);
 
-            Post post = new Post(city, state, category, title, description, user);
-            listPost.add(post);
-
-        }while (!cursor.moveToNext()); //exit loop if the cursor is already past the last entry in the result set.
-
+            } while (cursor.moveToNext()); //exit loop if the cursor is already past the last entry in the result set.
+        }
         return listPost;/*
 
         connect_func();
--- app/src/main/java/com/example/shareyourtrip/SearchActivity.java
+++ app/src/main/java/com/example/shareyourtrip/SearchActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,8 +16,11 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.List;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -57,6 +61,8 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        final PostDAO postDAO = new PostDAO(this);
 
         // Bottom navigation code
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
@@ -115,7 +121,27 @@ public class SearchActivity extends AppCompatActivity {
                         }
 
                         else {
-                            alertDisplay(SearchActivity.this, "Searhing for " + CategoryArray[position] + " in " + txt_City.getText().toString() + ", " + txt_State.getText().toString(), true);
+                            StringBuilder stringBuilder = new StringBuilder();
+                            stringBuilder.append("select * from post where ");
+                            stringBuilder.append("city='");
+                            stringBuilder.append(txt_City.getText().toString());
+                            stringBuilder.append("' and state='");
+                            stringBuilder.append(txt_State.getText().toString());
+                            stringBuilder.append("';");
+
+                            String[] cols = {"city", "state"};
+
+                            try {
+                                List<Post> listPost = postDAO.listAllPost(stringBuilder.toString(), null);
+                                if(listPost.size()==0){
+                                    alertDisplay(SearchActivity.this, "There is no post for the given criteria!", false);
+                                    //Toast.makeText(SearchActivity.this,"There are no posts with the given criteria!",Toast.LENGTH_LONG).show();;
+                                }
+                            }
+                            catch (SQLiteException e){
+                                Toast.makeText(SearchActivity.this,"There is a database problem!"+e.getMessage(),Toast.LENGTH_LONG).show();;
+                            }
+                            //alertDisplay(SearchActivity.this, "Searhing for " + CategoryArray[position] + " in " + txt_City.getText().toString() + ", " + txt_State.getText().toString(), true);
                         }
 
                     }
@@ -127,6 +153,5 @@ public class SearchActivity extends AppCompatActivity {
 
             }
         });
-
     }
 }
