c6fc33727f0d476d41e870169d65914d3640b639
= <=>
2020-04-10 18:26:36 -0400

    Prelimary tidying

--- app/src/main/java/com/example/shareyourtrip/PostAdapter.java
+++ app/src/main/java/com/example/shareyourtrip/PostAdapter.java
@@ -1,4 +1,5 @@
 package com.example.shareyourtrip;
+
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,120 +24,119 @@ import java.util.List;
 //Todo: Make fav button do something
 public class PostAdapter extends RecyclerView.Adapter<PostAdapter.MyViewHolder> {
 
-        //This will be a list of user posts to display to the users
-        private List<Post> postList;
+    //This will be a list of user posts to display to the users
+    protected List<Post> postList;
 
-        //This is a ViewHolder which holds 5 TextViews which make up our post.
-        public class MyViewHolder extends RecyclerView.ViewHolder {
-            public TextView location, category, title, description, user, date, likeCount, dislikecount;
-            public ToggleButton favButton, likeButton, dislikeButton;
+    //Copy constructor
+    public PostAdapter(List<Post> postList) {
+        this.postList = postList;
+    }
 
-            //Constructor of the ViewHolder, initializes TextViews
-            public MyViewHolder(View view) {
-                super(view);
-                location = (TextView) view.findViewById(R.id.location);
-                category = (TextView) view.findViewById(R.id.category);
-                title = (TextView) view.findViewById(R.id.title);
-                description = (TextView) view.findViewById(R.id.description);
-                user = (TextView) view.findViewById(R.id.user);
-                date = (TextView) view.findViewById(R.id.date);
-                favButton = (ToggleButton) view.findViewById(R.id.fav_button);
-                likeButton = (ToggleButton) view.findViewById(R.id.like_button);
-                likeCount = (TextView) view.findViewById(R.id.like_count);
-                dislikeButton = (ToggleButton) view.findViewById(R.id.dislike_button);
-                dislikecount = (TextView) view.findViewById(R.id.dislike_count);
-            }
-        }
+    //This is a ViewHolder which holds 5 TextViews which make up our post.
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView location, category, title, description, user, date, likeCount, dislikecount;
+        public ToggleButton favButton, likeButton, dislikeButton;
 
-        //Copy constructor
-        public PostAdapter(List<Post> postList) {
-            this.postList = postList;
-        }
-
-        //Returns a ViewHolder given the context its parent is in
-        @Override
-        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
-            View itemView = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.post_list_row, parent, false);
-            return new MyViewHolder(itemView);
-        }
-
-        //Once the ViewHolder is set in place, we retrieve the text from the UserPost
-        //and put it in the TextViews
-        @Override
-        public void onBindViewHolder(final MyViewHolder holder, int position) {
-
-            //Setting views with values from postList
-            Post post = postList.get(position);
-            holder.location.setText(post.getCity() + ", " + post.getState());
-            holder.category.setText(post.getCategory());
-            holder.title.setText(post.getTitle());
-            holder.description.setText(post.getDescription());
-            holder.user.setText(post.getUser());
-            holder.date.setText(post.getDate());
-            //todo holder.likeCount.setText( post.getLikes);
-            //todo holder.dislikeCount.setText( post.getDislikes );
-
-            holder.favButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                    //If favorite is toggled on...
-                    if (isChecked) {
-                        holder.favButton.setBackgroundResource(R.drawable.ic_favorite);
-                        //todo: Put favorite database stuff here
-                    }
-                    //If favorite is toggled off
-                    else {
-                        holder.favButton.setBackgroundResource(R.drawable.ic_unfavorited);
-                    }
-                }
-            });
-
-
-            //like/dislike logic
-            holder.likeButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                @Override
-                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                    //If like button toggled on
-                    if (isChecked){
-                        holder.likeButton.setBackgroundResource(R.drawable.ic_like_enabled);
-                        //..and dislike is toggled on
-                        if (holder.dislikeButton.isChecked()) {
-                            holder.dislikeButton.setChecked(false);//toggle off dislike.
-                        }
-                        //todo: Put like database stuff here
-                    }
-
-                    //If like button is toggled off
-                    else{
-                        holder.likeButton.setBackgroundResource(R.drawable.ic_like);
-                    }
-                }
-            });
-
-            holder.dislikeButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                @Override
-                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                    //If dislike toggled on
-                    if (isChecked){
-                        holder.dislikeButton.setBackgroundResource(R.drawable.ic_dislike_enabled);
-                        //...and if like is toggled on
-                        if (holder.likeButton.isChecked()) {
-                            holder.likeButton.setChecked(false);//toggle off like
-                        }
-                        //todo: Put dislike database stuff here
-                    }
-                    //If dislike is toggled off..
-                    else{
-                        holder.dislikeButton.setBackgroundResource(R.drawable.ic_dislike);
-                    }
-                }
-            });
-        }
-
-        //Returns size of list of posts
-        @Override
-        public int getItemCount() {
-            return postList.size();
+        //Constructor of the ViewHolder, initializes TextViews
+        public MyViewHolder(View view) {
+            super(view);
+            location = (TextView) view.findViewById(R.id.location);
+            category = (TextView) view.findViewById(R.id.category);
+            title = (TextView) view.findViewById(R.id.title);
+            description = (TextView) view.findViewById(R.id.description);
+            user = (TextView) view.findViewById(R.id.user);
+            date = (TextView) view.findViewById(R.id.date);
+            favButton = (ToggleButton) view.findViewById(R.id.fav_button);
+            likeButton = (ToggleButton) view.findViewById(R.id.like_button);
+            likeCount = (TextView) view.findViewById(R.id.like_count);
+            dislikeButton = (ToggleButton) view.findViewById(R.id.dislike_button);
+            dislikecount = (TextView) view.findViewById(R.id.dislike_count);
         }
     }
+
+    //Returns a ViewHolder given the context its parent is in
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.post_list_row, parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+    //Once the ViewHolder is set in place, we retrieve the text from the UserPost
+    //and put it in the TextViews
+    @Override
+    public void onBindViewHolder(final MyViewHolder holder, int position) {
+
+        //Setting views with values from postList
+        Post post = postList.get(position);
+        holder.location.setText(post.getCity() + ", " + post.getState());
+        holder.category.setText(post.getCategory());
+        holder.title.setText(post.getTitle());
+        holder.description.setText(post.getDescription());
+        holder.user.setText(post.getUser());
+        holder.date.setText(post.getDate());
+        //todo holder.likeCount.setText( post.getLikes);
+        //todo holder.dislikeCount.setText( post.getDislikes );
+
+        holder.favButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                //If favorite is toggled on...
+                if (isChecked) {
+                    holder.favButton.setBackgroundResource(R.drawable.ic_favorite);
+                    //todo: Put favorite database stuff here
+                }
+                //If favorite is toggled off
+                else {
+                    holder.favButton.setBackgroundResource(R.drawable.ic_unfavorited);
+                }
+            }
+        });
+
+        //like/dislike logic
+        holder.likeButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                //If like button toggled on
+                if (isChecked) {
+                    holder.likeButton.setBackgroundResource(R.drawable.ic_like_enabled);
+                    //..and dislike is toggled on
+                    if (holder.dislikeButton.isChecked()) {
+                        holder.dislikeButton.setChecked(false);//toggle off dislike.
+                    }
+                    //todo: Put like database stuff here
+                }
+
+                //If like button is toggled off
+                else {
+                    holder.likeButton.setBackgroundResource(R.drawable.ic_like);
+                }
+            }
+        });
+
+        holder.dislikeButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                //If dislike toggled on
+                if (isChecked) {
+                    holder.dislikeButton.setBackgroundResource(R.drawable.ic_dislike_enabled);
+                    //...and if like is toggled on
+                    if (holder.likeButton.isChecked()) {
+                        holder.likeButton.setChecked(false);//toggle off like
+                    }
+                    //todo: Put dislike database stuff here
+                }
+                //If dislike is toggled off..
+                else {
+                    holder.dislikeButton.setBackgroundResource(R.drawable.ic_dislike);
+                }
+            }
+        });
+    }
+
+    //Returns size of list of posts
+    @Override
+    public int getItemCount() {
+        return postList.size();
+    }
+}
